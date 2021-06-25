8a3d996e9e85810483952fd2aab8b01605039ad4
stanfrbd <44167150+stanfrbd@users.noreply.github.com>
2021-06-25 09:57:26 +0200

    Update setup_my_zsh.sh

--- setup_my_zsh.sh
+++ setup_my_zsh.sh
@@ -8,25 +8,15 @@ cat /etc/passwd | grep bash | cut -d ":" -f1 >> tmp.txt
 
 while read user
 do
-    sudo chsh -s /bin/zsh "$user"
+    su $user
+    cd
+    rm .vimrc .zshrc 2>/dev/null
+    chsh -s /bin/zsh $user
+    git clone https://github.com/zsh-users/zsh-autosuggestions ./.zsh/zsh-autosuggestions
+    wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.vimrc .
+    wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.zshrc .
 done < tmp.txt
 
 rm tmp.txt
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ./.zsh/zsh-autosuggestions
-
-wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.vimrc
-
-wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.zshrc
-
-sudo cd /root
-
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions ./.zsh/zsh-autosuggestions
-
-sudo wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.vimrc
-
-sudo wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.zshrc
-
-cd -
 
 echo "[>] $(tput setaf 2)Done. Leave and open the terminal again :)"
