890d360e7ac932512289192ac70e273130ce5fac
stanfrbd <44167150+stanfrbd@users.noreply.github.com>
2021-06-25 10:07:38 +0200

    Update setup_my_zsh.sh

--- setup_my_zsh.sh
+++ setup_my_zsh.sh
@@ -8,13 +8,18 @@ cat /etc/passwd | grep bash | cut -d ":" -f1 >> /tmp/tmp.txt
 
 while read user
 do
-    su $user
+    if [ "$user" == "root" ]
+    then
+        su
+    else
+        su $user
+    fi
     cd
     rm .vimrc .zshrc 2>/dev/null
     chsh -s /bin/zsh $user
     git clone https://github.com/zsh-users/zsh-autosuggestions ./.zsh/zsh-autosuggestions
-    wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.vimrc .
-    wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.zshrc .
+    wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.vimrc
+    wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.zshrc
 done < /tmp/tmp.txt
 
 rm /tmp/tmp.txt
