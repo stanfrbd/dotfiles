e73c7ab54bca4080cfa8ad404da00e7c1357969f
stanfrbd <44167150+stanfrbd@users.noreply.github.com>
2021-06-25 09:18:49 +0200

    Create setup_my_zsh.sh

---
+++ setup_my_zsh.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y zsh
+
+sudo -s
+cat /etc/passwd | grep bash | cut -d ":" -f1 >> tmp.txt
+
+while read user
+do
+    chsh -s /bin/zsh "$user"
+done < tmp.txt
+
+rm tmp.txt
+exit
+
+cd ~
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.vimrc
+
+https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.zshrc
+
+cd -
+
+echo "[>] $(tput setaf 2)Done. Leave and open the terminal again :)"
