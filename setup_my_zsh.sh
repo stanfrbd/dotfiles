f7a3a824a75a2a0da530a68085893a93daf48f38
stanfrbd <44167150+stanfrbd@users.noreply.github.com>
2021-08-02 09:13:20 +0200

    Fix certificate issues with wget in filtered envs

--- setup_my_zsh.sh
+++ setup_my_zsh.sh
@@ -5,7 +5,7 @@ sudo apt install vim -y
 sudo apt install git -y
 rm .vimrc .zshrc 2>/dev/null
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.vimrc --no-check-certificate
-wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.zshrc --no-check-certificate
+wget --no-check-certificate https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.vimrc
+wget --no-check-certificate https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.zshrc
 
 echo "[>] $(tput setaf 2)zsh setup completed for $(whoami). Launch zsh, leave and open the terminal again :)"
