30c2b2e9ec51e0c7db640a55a2243dc4c85bc052
stanfrbd <44167150+stanfrbd@users.noreply.github.com>
2021-08-02 09:05:38 +0200

    Update setup_my_zsh.sh

--- setup_my_zsh.sh
+++ setup_my_zsh.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cd
+sudo apt install vim -y
+sudo apt install git -y
 rm .vimrc .zshrc 2>/dev/null
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.vimrc
-wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.zshrc
+wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.vimrc --no-check-certificate
+wget https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.zshrc --no-check-certificate
 
-echo "[>] $(tput setaf 2)zsh put as default shell for $(whoami). Launch zsh, leave and open the terminal again :)"
+echo "[>] $(tput setaf 2)zsh setup completed for $(whoami). Launch zsh, leave and open the terminal again :)"
