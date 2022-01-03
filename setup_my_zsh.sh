ffa4676c243d7cc8854f8557384dee1ce81e6b6b
stanfrbd <stanislas.medrano@hotmail.com>
2022-01-03 11:55:14 +0100

    Updated setup_my_zsh.sh

--- setup_my_zsh.sh
+++ setup_my_zsh.sh
@@ -3,10 +3,11 @@
 cd
 sudo apt install vim -y
 sudo apt install git -y
-rm .vimrc .zshrc 2>/dev/null
+mv .vimrc .vimrc.bak
+mv .zshrc .zshrc.bak
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 wget --no-check-certificate https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.vimrc
 wget --no-check-certificate https://raw.githubusercontent.com/stanfrbd/dotfiles/main/.zshrc
-zsh
 echo "[>] $(tput setaf 2)zsh setup completed for $(whoami)."
+zsh
